ca89d5255338912b161ffde40479c96b6b6f530a
Sefafeteliyev <92524298+Sefafeteliyev@users.noreply.github.com>
2021-11-11 16:05:10 +0400

    Add files via upload

---
+++ 9/script.js
@@ -0,0 +1,76 @@
+// let cars = [ "bmw" , "ford" , "nissan" ];
+
+
+
+// cars.push(["opel" , "hyundai"]);
+
+
+// // console.log(cars[3][0]);
+
+// console.log(cars);
+
+
+
+// console.log(cars[1]);
+
+// cars.push("opel");
+
+// console.log(cars);
+
+// alert(cars.pop());
+
+// console.log(cars);
+
+// cars.unshift("opel");
+// console.log(cars);
+// alert(cars.shift());
+
+// console.log(cars);
+
+
+//  let cars = [ "bmw" , "ford" , "nissan" ];
+
+
+//  let eli = " " ;
+// let list=document.getElementById("list");
+// for(let i=0;i<cars.length;i++ ){
+//     eli+=`<li>${cars[i]}</li>`;
+// }
+// list.innerHTML=eli;
+
+// let cars = [ "bmw" , "ford" , "nissan" ];
+
+//  let eli = " " ;
+// let list=document.getElementById("list");
+
+// for(let key of cars){
+//     eli += `<li> ${key} </li>` 
+// }
+
+// list.innerHTML = eli;
+
+
+// let cars = [ "bmw" , "ford" , "nissan" ];
+
+//  let eli = " " ;
+// let list=document.getElementById("list");
+
+
+// for(let key in cars){
+//     eli+=`<li> ${cars[key]} </li>`;
+// }
+
+// list.innerHTML = eli;
+
+
+// let cars = [ "bmw" , "ford" , "nissan" ];
+
+//  let eli = " " ;
+// let list=document.getElementById("list");
+
+
+// cars.forEach( function(item){
+//     eli+= `<li> ${item} </li>`;
+// } )
+
+// list.innerHTML = eli;
